4a73008ac0a8bbe8d8b9a12b1cadccd533e495dc
ArchieOps <jonathanleonardo123@gmail.com>
2024-04-21 21:18:11 +0700

    First commit, add shared library files

---
+++ vars/buildImage.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t archieops/my-private-repo:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push archieops/my-private-repo:jma-2.0'
+}
